d580234d3aa8e8b71a36d33e12fe9a56d08b8f63
ManroopSingh <singh@manroop.com>
2017-04-12 17:15:54 -0400

    new branch for feature: new order request created

---
+++ app/src/main/java/com/example/singh/pizzachallenge/neworder/NewOrderActivity.java
@@ -0,0 +1,15 @@
+package com.example.singh.pizzachallenge.neworder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.singh.pizzachallenge.R;
+
+public class NewOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_order);
+    }
+}
